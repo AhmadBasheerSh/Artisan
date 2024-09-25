763de386b230e3fc853b5a226cf5911c5fe97d80
AhmadBasheer <ahmadabushawish@gamil.com>
2024-09-25 14:16:17 +0300

    main page design

---
+++ resources/views/index.blade.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>لمسات زخرفية | الصفحة الرئيسية</title>
+
+    <link rel="stylesheet" href="{{ asset('mainassets/css/index.css') }}">
+
+</head>
+<body>
+
+    <nav>
+        <ul class="list">
+            <li>
+                <a href="">الرئيسية</a>
+            </li>
+            <li>
+                <a href="">تسجيل</a>
+            </li>
+            <li>
+                <a href="">تسجيل الدخول</a>
+            </li>
+        </ul>
+
+        <a class="reem-kufi-fun logo" href="">لمسات زخرفية</a>
+
+        <ul>
+            <li>
+                <a href="">الرئيسية</a>
+            </li>
+            <li>
+                <a href="">تسجيل</a>
+            </li>
+            <li>
+                <a href="">تسجيل الدخول</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="contaner">
+
+        <h1 class="slider_text">شارك واحصل على أفكار</h1>
+        <div class="buttons-container">
+            <button class="circle-btn active"></button>
+            <button class="circle-btn"></button>
+            <button class="circle-btn"></button>
+            <button class="circle-btn"></button>
+        </div>
+
+        <p id="slide-title"> للتطريز والزخف وأفكار متنوعة</p>
+
+        <div class="d_animation" id="d1"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+        <div class="d_animation" id="d2"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+        <div class="d_animation" id="d3"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+        <div class="d_animation" id="d4"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+        <div class="d_animation" id="d5"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+        <div class="d_animation" id="d6"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+        <div class="d_animation" id="d7"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+        <div class="d_animation" id="d8"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+        <div class="d_animation" id="d9"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+        <div class="d_animation" id="d10"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+        <div class="d_animation" id="d11"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+        <div class="d_animation" id="d12"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+        <div class="d_animation" id="d13"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+    </div>
+    <div class="contaner2">
+        <div >
+            <h1 class="c2_title">ابحث عن اهتماماتك</h1>
+            <p class="c2_text">ماذا تريد أن تجرب بعد ذلك؟<br>
+                 فكر في شيء تحبه
+                 مثل "عشاء الدجاج السهل"<br>
+                 وشاهد ما ستجده.</p>
+        </div>
+        <div class="c2_img">
+            <div class="c2_img1"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+            <div class="c2_img2"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+            <div class="c2_img3"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+            <div class="c2_img4"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+        </div>
+    </div>
+    <div class="contaner3">
+        <div class="c3_img">
+            <div class="c3_img1"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+            <div class="c3_img2"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+            <div class="c3_img3"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+            <div class="c3_img4"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+            <div class="c3_img5"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>
+        </div>
+        <div>
+            <h1 class="c2_title">ابحث عن اهتماماتك</h1>
+            <p class="c2_text">ماذا تريد أن تجرب بعد ذلك؟<br>
+                 فكر في شيء تحبه
+                 مثل "عشاء الدجاج السهل"<br>
+                 وشاهد ما ستجده.</p>
+        </div>
+    </div>
+    <div class="contaner4">
+        <div>
+            <h1 class="c2_title">ابحث عن اهتماماتك</h1>
+            <p class="c2_text">ماذا تريد أن تجرب بعد ذلك؟<br>
+                 فكر في شيء تحبه
+                 مثل "عشاء الدجاج السهل"<br>
+                 وشاهد ما ستجده.</p>
+        </div>
+        <div class="c4_img">
+            {{-- <div class="c4_img1"><img src="{{ asset('mainassets/img/d2.jpg') }}" alt=""></div>        </div> --}}
+        <div>
+    </div>
+
+    <script src="{{ asset('mainassets/js/index.js') }}"></script>
+
+</body>
+</html>
